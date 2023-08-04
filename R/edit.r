e8a5f235a653205d35b78b92cf0cb152c9b9a71e
josephbrehm <64801701+josephbrehm@users.noreply.github.com>
2023-08-04 08:58:22 -0600

    Update edit.r
    fetch_edit: Add tall/wide options, force numeric data types, tidy whitespace

--- R/edit.r
+++ R/edit.r
@@ -6,14 +6,36 @@
 
 #' Fetch EDIT data 
 #' @description Fetch EDIT data
-#' @param mlra Character string or vector of character strings. The Major Land Resource Area (MLRA) or MLRAs to query. Only records from these MLRAs will be returned.
-#' @param data_type Restricted character string. The shorthand name of the type of data to retrieve. Must be one of climate", "ecodynamics", "general", "interpretations", "physiography", "reference", "soil", "supporting", "water", or "states"
-#' @param keys Optional character vector. A character vector of all the values to search for in \code{key_type}. The returned data will consist only of records where \code{key_type} contained one of the key values, but there may be keys that return no records. If \code{NULL} then the entire table will be returned. Defaults to \code{NULL}.
-#' @param key_type Optional character string. Variable to query using \code{keys}. Valid key_types are: precipitation, frostFreeDays, elevation, slope, landform, parentMaterialOrigin, parentMaterialKind, and surfaceTexture . Defaults to \code{NULL}
-#' @param key_chunk_size Numeric. The number of keys to send in a single query. Very long queries fail, so the keys may be chunked into smaller queries with the results of all the queries being combined into a single output. Defaults to \code{100}.
-#' @param timeout Numeric. The number of seconds to wait for a nonresponse from the API before considering the query to have failed. Defaults to \code{60}.
-#' @param path_unparsable_data Character string. If provided, then the function will save unparsable data to disk 
-#' @param verbose Logical. If \code{TRUE} then the function will report additional diagnostic messages as it executes. Defaults to \code{FALSE}.
+#' @param mlra Character string or vector of character strings. The Major Land 
+#' Resource Area (MLRA) or MLRAs to query. Only records from these MLRAs will be 
+#' returned.
+#' @param data_type Restricted character string. The shorthand name of the type 
+#' of data to retrieve. Must be one of: ecosites, climate, landforms, 
+#' physiography interval, physiography nominal, physiography ordinal, 
+#' annual production, overstory, understory, rangeland, surface cover, 
+#' parent material, soil interval, soil nominal, soil ordinal, 
+#' soil profile, texture, state narratives, or transition narratives.
+#' @param tall Logical. The function will output tall data if \code{TRUE}, 
+#' otherwise data will be in wide format. Defaults to \code{TRUE}. 
+#' @param keys Optional character vector. A character vector of all the values 
+#' to search for in \code{key_type}. The returned data will consist only of 
+#' records where \code{key_type} contained one of the key values, but there may 
+#' be keys that return no records. If \code{NULL} then the entire table will be 
+#' returned. Defaults to \code{NULL}.
+#' @param key_type Optional character string. Variable to query using 
+#' \code{keys}. Must be of of: precipitation, frostFreeDays, elevation, 
+#' slope, landform, parentMaterialOrigin, parentMaterialKind, or surfaceTexture.
+#' Defaults to \code{NULL}
+#' @param key_chunk_size Numeric. The number of keys to send in a single query. 
+#' Very long queries fail, so the keys may be chunked into smaller queries with 
+#' the results of all the queries being combined into a single output. Defaults 
+#' to \code{100}.
+#' @param timeout Numeric. The number of seconds to wait for a nonresponse from 
+#' the API before considering the query to have failed. Defaults to \code{60}.
+#' @param path_unparsable_data Character string. If provided, then the function 
+#' will save unparsable data to disk 
+#' @param verbose Logical. If \code{TRUE} then the function will report 
+#' additional diagnostic messages as it executes. Defaults to \code{FALSE}.
 
 #' @returns A data frame with the requested EDIT data. 
 #' 
@@ -22,23 +44,27 @@
 #' fetch_edit(mlra = "039X", data_type = "ecosites")
 #' # To retrieve ecological sites from MLRAs 039X and 040X
 #' fetch_edit(mlra = c("039X", "040X"), data_type = "ecosites")
-#' # To retrieve climatic feature descriptions from all ecological sites in MLRAs 039X and 040X
+#' # To retrieve climatic feature descriptions from all ecological sites in 
+#' MLRAs 039X and 040X
 #' fetch_edit(mlra = c("039X", "040X"), data_type = "climate")
-#' # To retrieve climatic feature descriptions from ecological sites that exist with slope between 15 and 30%, from MLRAs 039X and 040X.
-#' # Note: this includes all sites whose slope range overlaps with the given range. For example this will return sites with slope range 25-70%.
-#' fetch_edit(mlra = c("039X", "040X"), data_type = "climate", keys = "15:30", key_type = "slope")
+#' # To retrieve climatic feature descriptions from ecological sites that exist 
+#' with slope between 15 and 30%, from MLRAs 039X and 040X.
+#' # Note: this includes all sites whose slope range overlaps with the given 
+#' range. For example this will return sites with slope range 25-70%.
+#' fetch_edit(mlra = c("039X", "040X"), data_type = "climate", keys = "15:30", 
+#' key_type = "slope")
 #' 
 #' @rdname fetch_edit
 #' @export fetch_edit
-#' 
 
 fetch_edit <- function(mlra,
                        data_type,
+                       tall = TRUE,
                        keys = NULL,
                        key_type = NULL,
                        key_chunk_size = 100,
                        timeout = 60,
-                       verbose = F,
+                       verbose = FALSE,
                        path_unparsable_data = NULL){
   
   user_agent <- "http://github.com/Landscape-Data-Commons/trex"
@@ -222,52 +248,169 @@ fetch_edit <- function(mlra,
                         content_split <- gsub("\\\"", "", content_split)
                         
                         # Split into rows with the right number of tabs and those without
-                        content_goodrows <- content_split[sapply(sapply(content_split, strsplit, split = "(?<=.)(?=\t)", perl = T), length) == nheaders]
-                        content_badrows <- content_split[sapply(sapply(content_split, strsplit, split = "(?<=.)(?=\t)", perl = T), length) != nheaders]
+                        content_goodrows <- 
+                          content_split[sapply(sapply(content_split, 
+                                                      strsplit, 
+                                                      split = "(?<=.)(?=\t)", 
+                                                      perl = T), 
+                                               length) == nheaders]
+                        content_badrows <- 
+                          content_split[sapply(sapply(content_split, 
+                                                      strsplit, 
+                                                      split = "(?<=.)(?=\t)", 
+                                                      perl = T), 
+                                               length) != nheaders]
                         
                         # # Process the bad rows and write to a text file
                         if(length(content_badrows) > 0){
-                          ecosites_badrows <- sapply(1:length(strsplit(content_badrows, "\t")), function(n) {
-                            strsplit(content_badrows, "\t")[[n]][[2]]
-                          })
+                          ecosites_badrows <- 
+                            sapply(1:length(strsplit(content_badrows, "\t")), 
+                                   function(n) {
+                                     strsplit(content_badrows, "\t")[[n]][[2]]
+                                   })
                           
-                          ecosites_badrows <- unique(ecosites_badrows[ecosites_badrows != ""])
+                          ecosites_badrows <- 
+                            unique(ecosites_badrows[ecosites_badrows != ""])
                           
-                          if(length(ecosites_badrows) > 0){
-                            warning(paste0("Tabs present in data from following ecosites, making data from those ecosites unparsable. These data will be excluded from function output. \n"), 
-                                    paste(ecosites_badrows, collapse = ", "))
-                            if(!is.null(path_unparsable_data)){
-                              write.table(content_badrows, 
-                                          file.path(path_unparsable_data, 
-                                                    paste0("unparsable_data_", mlra, "_", Sys.Date(), ".txt")), 
-                                          row.names = F, 
-                                          col.names = F)
-                            } else {
-                              warning("path_unparsable_data not provided, unparsable data will not be saved.")
-                            }
+                          warning(paste0("Tabs present in data from following ecosites, making data from those ecosites unparsable. \n"), 
+                                  paste(paste(ecosites_badrows, collapse = ", ")), "\n",
+                                  "These data will be excluded from function output. If path_unparsable_data was provided, function will save these data to a text file.")
+                          if(!is.null(path_unparsable_data)){
+                            write.table(content_badrows, 
+                                        file.path(path_unparsable_data, 
+                                                  paste0("unparsable_data_", mlra, "_", Sys.Date(), ".txt")), 
+                                        row.names = F, 
+                                        col.names = F)
+                          } else {
+                            warning("path_unparsable_data not provided, unparsable data will not be saved.")
                           }
-                          
                         }
                         
-                        
                         # Now that we're rid of the rows with tabs in the data, split by tabs
-                        content_splitfinal <- unlist(strsplit(content_goodrows, split = "(?<=.)(?=\t)", perl = T))
-                        # content_splitfinal <- unlist(strsplit(content_goodrows, split = "[\t]"))
+                        content_splitfinal <- unlist(strsplit(content_goodrows, 
+                                                              split = "(?<=.)(?=\t)", 
+                                                              perl = T))
                         
-                        # # Remove tabs (which are retained by the above)
+                        # Remove tabs (which are retained by the above)
                         content_splitfinal <- gsub("\t", "", content_splitfinal)
                         
                         # Convert string into a data frame
-                        content_df <- as.data.frame(matrix(content_splitfinal, ncol = nheaders, byrow = T))
+                        content_df <- as.data.frame(matrix(content_splitfinal, 
+                                                           ncol = nheaders, 
+                                                           byrow = T))
                         colnames(content_df) <- headers
                         
                         # Due to processing everything as a character, string '"NA"' is present and must be removed
-                        content_df <- as.data.frame(sapply(content_df, function(r) gsub("^NA$", NA, r)))
+                        content_df <- 
+                          as.data.frame(sapply(content_df,
+                                               function(r) 
+                                                 gsub("^NA$", NA, r)))
                         
                         return(content_df)
                       })
   
+  necosites <- sum(sapply(data_list, length))
+  
+  # If no data is present, stop the function
+  if(necosites == 0){
+    stop("No data returned")
+  }
+  
+  # Turn the list of data frames into one data frame
   results_dataonly <- do.call(rbind, data_list)
-  return(results_dataonly)
+  
+  # Enforce numeric type
+  results_dataonly <- suppressWarnings(
+    results_dataonly %>% 
+      dplyr::mutate_if(names(.) %in% c(
+        "Representative low", 
+        "Representative high",
+        "Range low", 
+        "Range high", 
+        "Average", 
+        "Production low", 
+        "Production RV", 
+        "Production high",
+        "Foliar cover low", 
+        "Foliar cover high",
+        "Cover low", 
+        "Cover high",
+        "Top depth", 
+        "Bottom depth"),
+        as.numeric))
+  
+  # Tall output is ready
+  if(tall){
+    return(results_dataonly)
+  } else {
+    # Pivot data if tall is FALSE
+    if(data_type %in% c("landforms", "ecosites", "parent material", "texture", "state narratives", "transition narratives")){ # No pivot needed
+      results_pivot <- results_dataonly 
+    } else if(data_type == "climate"){
+      # Measurement unit is often left blank
+      results_dataonly[results_dataonly$`Measurement unit` == "" | 
+                         is.na(results_dataonly$`Measurement unit`) | 
+                         is.null(results_dataonly$`Measurement unit`), 
+                       "Measurement unit"] <- "unknown unit"
+      
+      results_pivot <- tidyr::pivot_wider(results_dataonly, 
+                                          id_cols = c("MLRA", "Ecological site ID", "Ecological site legacy ID"),
+                                          names_from = c("Property", "Measurement unit"), 
+                                          values_from = c("Representative low", "Representative high", "Range low", "Range high", "Average"))
+      
+    } else if(data_type %in% c("physiography interval", "soil interval")){
+      results_dataonly[results_dataonly$`Measurement unit` == "" | 
+                         is.na(results_dataonly$`Measurement unit`) | 
+                         is.null(results_dataonly$`Measurement unit`),
+                       "Measurement unit"] <- "unknown unit"
+      
+      results_pivot <- tidyr::pivot_wider(results_dataonly,
+                                          id_cols = c("MLRA", "Ecological site ID", "Ecological site legacy ID"),
+                                          names_from = c("Property", "Measurement unit"),
+                                          values_from = c("Representative low", "Representative high", "Range low", "Range high"))
+      
+    } else if(data_type %in% c("physiography ordinal", "soil ordinal")){
+      results_pivot <- tidyr::pivot_wider(results_dataonly,
+                                          id_cols = c("MLRA", "Ecological site ID", "Ecological site legacy ID"),
+                                          names_from = c("Property"),
+                                          values_from = c("Representative low", "Representative high", "Range low", "Range high"))
+      
+    } else if(data_type %in% c("physiography nominal", "soil nominal")){
+      results_pivot <- tidyr::pivot_wider(results_dataonly,
+                                          id_cols = c("MLRA", "Ecological site ID", "Ecological site legacy ID"),
+                                          names_from = c("Property"),
+                                          values_from = c("Property value"))
+    } else if(data_type %in% c("annual production")){
+      results_pivot <- tidyr::pivot_wider(results_dataonly,
+                                          id_cols = c("MLRA", "Ecological site ID", "Ecological site legacy ID", "Land use", "Ecosystem state", "Plant community"),
+                                          names_from = c("Plant type"),
+                                          values_from = c("Production low", "Production RV", "Production high"))
+      
+    } else if(data_type %in% c("rangeland")){
+      results_pivot <- tidyr::pivot_wider(unique(results_dataonly),
+                                          id_cols = c("MLRA", "Ecological site ID", "Ecological site legacy ID", "Land use", "Ecosystem state", "Plant community", "Custom group number"),
+                                          names_from = c("Plant symbol"),
+                                          values_from = c("Production low", "Production high", "Foliar cover low", "Foliar cover high"))
+      
+    } else if(data_type %in% c("surface cover")){
+      results_pivot <- tidyr::pivot_wider(unique(results_dataonly),
+                                          id_cols = c("MLRA", "Ecological site ID", "Ecological site legacy ID", "Land use", "Ecosystem state", "Plant community"),
+                                          names_from = c("Cover type"),
+                                          values_from = c("Cover low", "Cover high"))
+      
+    } else if(data_type %in% c("soil profile")){
+      results_dataonly[results_dataonly$`Measurement unit` == "" | 
+                         is.na(results_dataonly$`Measurement unit`) | 
+                         is.null(results_dataonly$`Measurement unit`),
+                       "Measurement unit"] <- "unknown unit"
+      
+      results_pivot <- tidyr::pivot_wider(unique(results_dataonly),
+                                          id_cols = c("MLRA", "Ecological site ID", "Ecological site legacy ID"),
+                                          names_from = c("Property", "Measurement unit"),
+                                          values_from = c("Top depth", "Bottom depth", "Representative low", "Representative high", "Range low", "Range high"))
+      
+    }
+    
+    return(results_pivot)
+  }
 }
-
