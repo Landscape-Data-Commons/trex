1987e3dae60f3d58ebb53f85d6d7c33a71d82d2e
Nelson Stauffer <nelson.stauffer@usda.gov>
2026-03-27 15:20:09 -0700

    Update credential handling

--- R/credentials.R
+++ R/credentials.R
@@ -46,7 +46,10 @@ get_trex_keyring_name <- function(username,
 
 #' Establish a user-specific trex keyring if possible
 #' @description
-#' Attempt to create a keyring for the user to store their API keys in.
+#' Attempt to create a keyring for the user to store their API keys in. The keyring name is created using \code{generate_keyring_name()}.
+#' @param username Character string. The username associated with the LDC API account. This is almost certainly an email address.
+#' @param keyring_password OPTIONAL (and discouraged) character string. The password to use to encrypt the keyring and which will be used to access and API keys stored in the keyring in the future. It is very strongly recommended that you never save a plaintext password in your code. If this is \code{NULL} and \code{interactive} is \code{TRUE} then you will be prompted to type a password when the function is run. Defaults to \code{NULL}.
+#' @param interactive Logical. If \code{TRUE} and either \code{username} or \code{keyring_password} is \code{NULL} then interactive prompts will ask you for the missing values. Defaults to \code{TRUE}.
 #' @param verbose Logical. If \code{TRUE} the function will produce diagnostic
 #'   messages. Defaults to \code{FALSE}.
 setup_keyring <- function(username = NULL,
@@ -76,6 +79,8 @@ setup_keyring <- function(username = NULL,
         } else {
           stop("In order to create a new keyring for trex, a password must be set. Please either set the argument interactive to TRUE (preferred) or provide the password as the argument keyring_password (not recommended).")
         }
+      } else {
+        warning("You provided a keyring password as an argument. Make absolutely sure that that password is not saved in plaintext anywhere!")
       }
       
       if (verbose) {
@@ -95,10 +100,18 @@ setup_keyring <- function(username = NULL,
   }
 }
 
+#' Securely store an API key for future use
+#' @description
+#' Store an API key in the user's encrypted keyring. The user must provide the
+#' key when interactively prompted. Other trex functions for requesting data from
+#' an API will use their own \code{username} and \code{api_key_name} to access keys
+#' stored in this way.
+#' 
 #' @param username Character string. The username associated with the API key being stored, typically an email address.
 #' @param api_key_name Character string. The name to store the API key under in the keyring. Defaults to \code{"default"}.
 #' @param overwrite Logical. If \code{TRUE} then an existing key stored with the name provided as \code{api_key_name} will be overwritten. Defaults to \code{FALSE}.
-#' @param 
+#' @param verbose Logical. If \code{TRUE} the function will produce diagnostic
+#'   messages. Defaults to \code{FALSE}.
 store_api_key <- function(username,
                           # keyring_password = NULL,
                           # api_key = NULL,
@@ -146,10 +159,25 @@ store_api_key <- function(username,
 }
 
 
-
+#' Retrieve a securely-stored API key
+#' @description
+#' Retrieve a previously-stored API key from the user's encrypted keyring.
+#' There is no argument for the keyring password and the user must provide it when interactively prompted when this function is run and the keyring is locked.
+#' The keyring is managed by the user's OS and will remain unlocked for a period of time controlled by the OS, so the user will not always be prompted for a password.
+#' Other trex functions for requesting data from
+#' an API will use their own \code{username} and \code{api_key_name} to access keys
+#' stored in this way.
+#' 
+#' @param username Character string. The username associated with the API key being stored, typically an email address.
+#' @param api_key_name Optional character string. The name that the API key was stored under in the keyring. Defaults to \code{NULL} which will attempt to retrieve a key called \code{"efault"}.
+#' @param keyring_name Optional character string. The name of the keyring that the API key was stored in. If this is \code{NULL} then the keyring matching \code{generate_keyring_name(username)} will be used. Defaults to \code{NULL}.
+#' @param accept_failure Logical. If \code{TRUE} then the function will return \code{NULL} if a key can't be found. Otherwise an error message will be produced. Defaults to \code{TRUE}.
+#' @param verbose Logical. If \code{TRUE} the function will produce diagnostic
+#'   messages. Defaults to \code{FALSE}.
 get_stored_key <- function(username,
                            api_key_name = NULL,
                            keyring_name = NULL,
+                           accept_failure = TRUE,
                            verbose = FALSE) {
   
   if (is.null(api_key_name)) {
@@ -174,7 +202,24 @@ get_stored_key <- function(username,
             keyring_name, "'.")
   }
   
-  keyring::key_get(service = api_key_name,
+  output <- keyring::key_get(service = api_key_name,
                    username = username,
-                   keyring = keyring_name)
+                   keyring = keyring_name) |>
+    tryCatch(expr = _,
+             error = function(error_message){
+               !stringr::str_detect(string = error_message$message,
+                                    pattern = "Element not found")
+             })
+  
+  if (!is.character(output)) {
+    if (accept_failure & !output) {
+      output <- NULL
+    } else if (!accept_failure & !output) {
+      stop("Unable to find the specified API key.")
+    } else if (output) {
+      stop("Some kind of error occurred with retrieving the stored API key. Double check your username and api_key_name values.")
+    }
+  }
+  
+  output
 }
